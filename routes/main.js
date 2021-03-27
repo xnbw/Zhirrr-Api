3c6ff3d156d35131c9f13b0559b489c039c4af10
xnbw <74663434+xnbw@users.noreply.github.com>
2021-03-27 08:44:25 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,14 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/home.html')
+    res.sendFile(__path + '/views/index.html'')
 })
 
-router.get('/api', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
-})
-
-router.get('/api/about', (req, res) => {
+router.get('/about', (req, res) => {
     res.sendFile(__path + '/views/about.html')
 })
 
